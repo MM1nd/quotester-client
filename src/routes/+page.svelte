2a6baedd35fa1a6dbf30048117d8ef783bfea4e3
MM1nd <alexkornrumpf@web.de>
2024-11-19 00:40:27 +0100

    Prepare Card Component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script>
+	import Card from '../components/Card.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="flex flex-row flex-wrap">
+	<Card></Card>
+	<Card></Card>
+	<Card></Card>
+</div>
